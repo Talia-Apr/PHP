e81ae71e55745bb5de1049816e89256356e13aa4
Talia-Apr <taliaaprianti46@gmail.com>
2020-10-14 18:19:19 +0700

    Tugas Minggu ke 14

---
+++ PHP-SMK 41-45/restoran/user/updateuser.php
@@ -0,0 +1,75 @@
+<?php 
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        $sql = "SELECT * FROM tbluser WHERE iduser=$id";
+        $row = $db -> getITEM($sql);
+    }
+
+?>
+
+<h3>UPDATE USER</h3>
+
+<div class="form-group">
+    <form action="" method="post">
+
+    <!-- Form Pertama -->
+    <div class="form-group w-50">
+        <label for="">Nama User</label>
+        <input type="text" name="user" required value="<?php echo $row['user']?>" class="form-control">
+    </div>
+
+    <!-- Form Kedua -->
+    <div class="form-group w-50">
+        <label for="">Email</label>
+        <input type="text" name="email" required value="<?php echo $row['email']?>" class="form-control">
+    </div>
+
+    <!-- Form Ketiga -->
+    <div class="form-group w-50">
+        <label for="">Password</label>
+        <input type="password" name="password" required value="<?php echo $row['password']?>" class="form-control">
+    </div>
+
+    <!-- Form Keempat -->
+    <div class="form-group w-50">
+        <label for="">Konfirmasi Password</label>
+        <input type="password" name="konfirmasi" required value="<?php echo $row['password']?>" class="form-control">
+    </div>
+
+    <!-- Combobox -->
+    <div class="form-group w-50">
+        <label for="">Level</label>
+        <br>
+        <select name="level" id="">
+            <option value="admin"<?php if($row['level'] === "admin") echo "selected" ?>>Admin</option>
+            <option value="koki" <?php if($row['level'] === "koki") echo "selected" ?>>Koki</option>
+            <option value="kasir" <?php if($row['level'] === "kasir") echo "selected" ?>>Kasir</option>
+        </select>
+    </div>
+
+    <!-- Button -->
+    <div>
+        <input type="submit" name="simpan" value="simpan" class="btn btn-primary">
+    </div>
+    </form>
+</div>
+
+<?php 
+    if (isset($_POST['simpan'])) {
+        $user = $_POST['user'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $konfirmasi = $_POST['konfirmasi'];
+        $level = $_POST['level'];
+
+        if ($password == $konfirmasi) {
+            $sql = "UPDATE tbluser SET user='$user', email='$email', password='$password', level='$level' WHERE iduser = $id";
+            $db->runSQL($sql);
+            header("location:?f=user&m=select");
+        } else {
+            echo '<h3>Password Tidak Sama!</h3>';
+        }
+    }
+?>
